1c39ae3e39c662b403ebe762d4c06aba0623d44c
Alen Toma <alen.toma@gmail.com>
2025-11-01 02:09:32 +0100

    bug fix

--- src/parsers/ParserWrapper.ts
+++ src/parsers/ParserWrapper.ts
@@ -217,7 +217,7 @@ export default class ParserWrapper extends Parser {
     isDebug: debugg,
     folder: (target: Parser) => target.name,
     keyModifier: (target, key) => `group_${key}${target.name}`,
-    argsOverride:(args)=> [args[1]],
+    argsOverride:(args)=> [args[0], args[1]],
     validator: (data: any) => data && data.has() && !data[0].name.empty() && !data[0].url.empty()
   })
   async group(
